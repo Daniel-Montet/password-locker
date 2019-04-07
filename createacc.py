dffd4dc702d1916b143bd054b7c7b68b94cbdd85
Daniel Montet <dmontetproff@gmail.com>
2019-04-07 18:24:07 +0300

    Add error assersion test

--- createacc.py
+++ createacc.py
@@ -1,6 +1,36 @@
+print("""
+            Welcome to the password locker app :) 
+            You first of all need to create an account
+            """)
+
 class CreateAcc:
     def __init__(self, name="",password="", **kwargs):
-        self.name = name
 
-        for key,value in kwargs.items():
-            setattr(self,key,value)
+        try:
+            
+            self.name= input(">Username:  ")
+            self.password= input(">Password:  ")
+            
+            if int(len(self.name)) < 1:
+                raise ValueError("Name is required")
+
+            if int(len(self.password)) < 6:
+                raise ValueError("Password must be 6 characters or more")
+
+        except ValueError as err:
+            print("""something went wrong, :(
+                {} !!
+                
+                """.format(err))
+
+
+        else:
+            self.name = name
+            self.password = password
+            for key,value in kwargs.items():
+                setattr(self,key,value)
+        
+      
+        
+
+        
--- test.py
+++ test.py
@@ -8,10 +8,15 @@ from createacc import CreateAcc
 class CreateTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_account= CreateAcc("Daniel")
+        self.new_account= CreateAcc("","4455")
     
     def test_init(self):
-        self.assertEqual(self.new_account.name,"Daniel") 
+        self.assertEqual(self.new_account.name,"")
+        self.assertEqual(self.new_account.password,"")
+    
+    def test_err(self):
+        with self.assertRaises(ValueError):
+            self.new_account.password
 
 if __name__=='__main__':
     unittest.main()
