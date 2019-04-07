5f6e75803bd51c639a7d3904ceffd111a5e8bd78
Daniel Montet <dmontetproff@gmail.com>
2019-04-07 22:23:41 +0300

    Add creation of user instance on login

--- login.py
+++ login.py
@@ -1,12 +1,30 @@
 import os
 import time
+from user import User
+from credential import Credential
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-time.sleep(1)
 
 print("""
             Welcome to the password locker app :) 
             You first of all need to create an account
             """)
+
+time.sleep(5)
+clear_screen()
+
+print("You will need a username and password")
+
+time.sleep(2)
+clear_screen()
+
+name= input("Name: ")
+
+time.sleep(3)
+clear_screen()
+
+password = input("Password:  ")
+
+
