277061be473c93781c50658210a508174ff767b8
Daniel Montet <dmontetproff@gmail.com>
2019-04-08 08:35:09 +0300

    Add random password generator function

--- login.py
+++ login.py
@@ -1,5 +1,7 @@
 import os
 import time
+import random
+import string
 from user import User
 from credential import Credential
 
@@ -12,31 +14,31 @@ print("""
             You first of all need to create an account
             """)
 
-time.sleep(4)
-clear_screen()
+#time.sleep(2)
+#clear_screen()
 
 print("You will need a username and password")
 
-time.sleep(2)
-clear_screen()
+#time.sleep(2)
+#clear_screen()
 
 name= input("Name: ")
 
-time.sleep(3)
-clear_screen()
+#time.sleep(1)
+#clear_screen()
 
 password = input("Password:  ")
 
-time.sleep(3)
-clear_screen()
+#time.sleep(1)
+#clear_screen()
 
 print("Your have successfully created an account")
 
 createduser= Credential(name,password)
 createduser.account.update({"login":[name,password]})
 
-time.sleep(3)
-clear_screen()
+#time.sleep(1)
+#clear_screen()
 
 print("Log in to continue :)")
 
@@ -44,16 +46,26 @@ def add_credential(social,username,password):
     createduser.account.update({social:[username,password]})
 def display_credential(social):
     return social
+def delete_credential(social):
+
+def generate_password(stringLength):
+    """Generate a random string of letters and digits """
+    lettersAndDigits = string.ascii_letters + string.digits
+    password=''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+    return password
+
 
 while True:
     loginname = input(">Username:  ")
     loginpwd = input(">Password:  ")
 
     try:
-        if loginname !== createduser.account[login][0]:
+        if loginname != createduser.account['login'][0]:
             raise ValueError("This is not a recognised user name !!")
-        if loginpwd !== createduser.account[login][1]:
+            continue
+        if loginpwd != createduser.account['login'][1]:
             raise ValueError("Incorrect password !!")
+            continue
     except ValueError as err:
         print("""Something is wrong :(
             {}
@@ -61,4 +73,20 @@ while True:
             """.format(err))
     else:
         print("Welcome {}. You have successfully logged in".format(name))
-        
+        break
+
+print("""
+Enter a credential you need added:
+
+""")
+
+social= input("> Social name e.g. Facebook:   ")
+username= input("> Username:  ")
+socialpwd= input("> Password: Enter 'auto' to autogenerate a password: >   ")
+
+while True:
+    if socialpwd == 'auto':
+        print("Please wait as we generate password for you")
+
+
+
